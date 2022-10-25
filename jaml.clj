00111d6ff01fac21b97fb6a4cc1f51a3d999ce9f
Rune Juhl (Atea) <rune.juhl@atea.dk>
2022-10-25 20:37:00 +0200

    JSON to YAML converter

---
+++ jaml.clj
@@ -0,0 +1,10 @@
+#!/usr/bin/env bb
+(ns yaml
+  (:require [cheshire.core :as json]
+            [clj-yaml.core :as yaml]
+            [clojure.java.io]))
+
+(print (yaml/generate-string
+          (json/parse-stream (clojure.java.io/reader
+                              (first *command-line-args*)))
+          :dumper-options {:flow-style :block}))
